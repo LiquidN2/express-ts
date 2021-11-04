64589c0051ff45dd67ab84ee31fdf0c3782858a5
Hugh Nguyen <hhnguyen255@gmail.com>
2021-11-04 17:37:36 +1100

    limit routbinder decorators to only use with request handler

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,13 +1,21 @@
 import 'reflect-metadata';
+import { RequestHandler } from 'express';
 import { Methods } from './Methods';
 import { MetadataKeys } from './MetadataKeys';
 
 type Decorator = (target: any, key: string, desc: PropertyDescriptor) => void;
 type DecoratorGenerator = (path: string) => Decorator;
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
 
 function routeBinder(requestMethod: string): DecoratorGenerator {
   return function (path: string): Decorator {
-    return function (target: any, key: string, desc: PropertyDescriptor): void {
+    return function (
+      target: any,
+      key: string,
+      desc: RouteHandlerDescriptor
+    ): void {
       Reflect.defineMetadata(MetadataKeys.path, path, target, key);
       Reflect.defineMetadata(MetadataKeys.method, requestMethod, target, key);
     };
